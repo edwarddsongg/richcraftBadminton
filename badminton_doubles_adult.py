88c44276ae6e2ba2dd931a8fb0723c564557d53f
edward-song-life-is-short <ybob566@gmail.com>
2023-01-22 21:19:33 -0500

    Added support for multiple acitvities

--- badminton_doubles_adult.py
+++ badminton_doubles_adult.py
@@ -11,6 +11,25 @@ def find_date(class_name, browser):
     dates = browser.find_elements(By.CLASS_NAME, class_name)
     available = len(dates)
 
+    week_dates = browser.find_elements(By.CLASS_NAME, "title")
+    print(len(week_dates))
+    if not len(week_dates):
+        print("There are no times this week")
+        return
+    else:
+        print("why")
+        for date in week_dates:
+            date.click()
+
+    time.sleep(1)
+
+    reservation_count = browser.find_element(By.ID, "reservationCount")
+    reservation_count.clear()
+    reservation_count.send_keys(2)
+
+    button = browser.find_element(By.ID, "submit-btn")
+    button.click()
+
     if not available:
         print("There are no more available dates")
     else:
@@ -40,8 +59,7 @@ def find_date(class_name, browser):
             cur_link = browser.current_url
 
     
-
-def doubles_adult(browser):
+def doubles_adult(browser, activity):
     dateTime = time.strftime("%H:%M")
 
     link = "https://reservation.frontdesksuite.ca/rcfs/richcraftkanata/Home/Index?Culture=en&PageId=b3b9b36f-8401-466d-b4c4-19eb5547b43a&ShouldStartReserveTimeFlow=False&ButtonId=00000000-0000-0000-0000-000000000000"
@@ -49,18 +67,11 @@ def doubles_adult(browser):
     
     browser.get(link)
 
-    button = browser.find_element(By.LINK_TEXT, "Badminton doubles - adult")
+    button = browser.find_element(By.LINK_TEXT, activity)
     button.click()
     time.sleep(1)
-
-    reservation_count = browser.find_element(By.ID, "reservationCount")
-    reservation_count.clear()
-    reservation_count.send_keys(2)
-
-    button = browser.find_element(By.ID, "submit-btn")
-    button.click()
-
+   
     find_date("date-text", browser)
 
-    time.sleep(5)
+    time.sleep(20)
 
--- main.py
+++ main.py
@@ -3,9 +3,36 @@ from badminton_doubles_adult import doubles_adult
 from webdriver_manager.chrome import ChromeDriverManager
 
 def main():
+    
+    activity = ""
+    select = 5
+    print("Which activity do you want?\n1. Badminton Family") 
+    print("2. Badminton Adult \n3. Badminton All Ages\n4. Swimming" )
+
+    while select < 0 or select > 4:
+        select = int(input(""))
+
+        if not select:
+            activity = "Badminton - Family"
+            break
+        elif select == 1:
+            activity = "Badminton doubles - adult"
+            break
+        elif select == 2:
+            activity = "Badminton doubles - adult"
+            break
+        elif select == 3:
+            activity = "Badminton doubles - all ages"
+            break
+        elif select == 4:
+            activity = "Lane swim"
+            break
+
+
     browser = webdriver.Chrome(ChromeDriverManager().install())
-    print("yp")
-    doubles_adult(browser) 
+    print("to")
+    print(activity)
+    doubles_adult(browser, activity) 
 
 if __name__ == "__main__":
     main()
